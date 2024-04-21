384d1b146c102324fbfac2b6c184ccc161fa197e
Aaron storey <aaron.storey@hotmail.co.uk>
2024-04-21 21:16:45 +0000

    Feat: the abouts app is now all linked up and veiws now work

--- about/models.py
+++ about/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-#class AboutMe (models.Model):
-#    title = models.CharField(max_length=200, unique=True)
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
